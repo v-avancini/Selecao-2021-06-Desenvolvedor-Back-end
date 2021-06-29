bbf6730b6f105b183c787dfd39e80aa0207a4f67
Vitor <avancini.cabral@gmail.com>
2021-06-28 22:20:19 -0300

    Erros encontrados e solucionados. Respostas no arquivo readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Selecao-2021-06-Desenvolvedor-Back-end
+
+#Seleção 2021-06 - Desenvolvedor Backend
+
+##Este documento faz parte da segunda etapa do processo seletivo para desenvolvedor backend para a empresa Bertholdo
+
+Erros Encontrados
+=================
+
+    * Erro 1:
+        *Linha 5: no form estava "idex.php", o correto é "index.php"
+
+    * Erro 2:
+        *Linha 18: variável errada ($cp), a correta é $cep
+        
+    * Erro 3:
+        *Linha 21: address escrito errado, com apenas 1 s
+                
+    * Erro 4:
+        *Linha 21: logradouro escrito errado, faltando letra o
+        
+    * Erro 5:
+        *Linha 23: address escrito errado, com apenas 1 d
+        
+    * Erro 6:
+        *Linha 30: faltando uma barra separando ws da variável cep
+
+
+
+
+
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<title> MEU CEP </title>
 	<body> 
-		<form action="idex.php" method="post">
+		<form action="index.php" method="post"> <!-- 1: form estava "idex.php", o correto é "index.php" -->
 		<label> Insira o CEP: </label>
 		<input type="text" name="cep">
 		<input type="submit" value="Enviar">
@@ -15,19 +15,19 @@ if(!empty($_POST['cep'])){
 	
 	$cep = $_POST['cep'];
 
-	$address = (get_address($cp));
+	$address = (get_address($cep));//2: variável errada ($cp), a correta é $cep
 
 	echo "<br><br>CEP Informado: $cep<br>";
-	echo "Rua: $addres->logradoro<br>";
+	echo "Rua: $address->logradouro<br>"; //3: adress escrito errado, com apenas 1 s //4: logradouro escrito errado, faltando letra o
 	echo "Bairro: $address->bairro<br>";
-	echo "Estado: $adress->uf<br>";
+	echo "Estado: $address->uf<br>"; //5: addres escrito errado, com apenas 1 d
 }
 
 function get_address($cep){
 	
 	
 	$cep = preg_replace("/[^0-9]/", "", $cep);
-	$url = "http://viacep.com.br/ws$cep/xml/";
+	$url = "http://viacep.com.br/ws/$cep/xml/";//6: faltando uma barra separando ws do cep
 
 	$xml = simplexml_load_file($url);
 	return $xml;
